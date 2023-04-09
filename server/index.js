6071d39828f7bb9392f6785e923f1893c483aace
unknown <codingbucketorg@gmail.com>
2023-04-09 11:17:59 +0500

    first commit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from 'cors'
 import connectDb from './mongoDb/connect.js';
 import postRoutes from './routes/postRoutes.js'
 import dalleRoutes from './routes/dalleRoutes.js'
-import path from 'path'
+
 dotenv.config();
 
 const app = express();
@@ -15,10 +15,7 @@ app.use(express.json({limit:'50mb'}))
 app.use('/api/v1/posts',postRoutes)
 app.use('/api/v1/dalle',dalleRoutes)
 
-app.use(express.static(path.join(__dirname,'../client/dist')))
-app.get('*',function(req,res){
-    res.sendFile(path.join(__dirname,'../client/dist/index.html'))
-})
+
 
 app.get('/',async (req,res)=>{
     res.send('Hello from DALLE')
